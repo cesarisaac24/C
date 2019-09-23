db813fd41bcbefc3650f1531652eb0536dfa05ab
c-cesarisaac24 <cesarisaac24@gmail.com>
2019-09-23 13:41:54 -0500

    for
    for

---
+++ Ejercicios/for/For.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+ int main() {
+    int cont;
+   
+   for (cont=1;cont<=50;cont++)
+   {
+   	printf ("%d: debo hacer mis tareas\n", cont);
+   }
+   getchar();
+   return 0;
+}
+
